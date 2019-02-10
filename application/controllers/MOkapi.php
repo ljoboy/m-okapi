f171943b152b0b769a005673e77349e64ae127d8
Dark Angel <jonathanyombo@gmail.com>
2019-02-10 22:05:15 +0200

    add mdp_change_action method

--- application/controllers/MOkapi.php
+++ application/controllers/MOkapi.php
@@ -22,7 +22,25 @@ class MOkapi extends CI_Controller
 
 	public function mdp_change_action()
 	{
-		
+		$this->load->model('utilisateurModel');
+		$this->form_validation->set_rules('actuel', 'ancien mot de passe', 'required', ['required' => 'Ce champ est requis']);
+		$this->form_validation->set_rules('new_mdp', 'nouveau mot de passe', 'required', ['required' => 'Ce champ est requis']);
+		$this->form_validation->set_rules('confirmer', 'confirmer mot de passe', 'required|matches[new_mdp]',
+			['required' => 'Ce champ est requis', 'matches' => 'Mot de passes discordent']);
+
+		$ex_mdp = $this->input->post('actuel', TRUE);
+		$mdp = $this->input->post('new_mdp', TRUE);
+
+		if ($this->form_validation->run() == true) {
+			if ($this->utilisateurModel->change_mdp($ex_mdp, $mdp)){
+				$this->session->set_flashdata('success_mdp_change', 'Mot de passe changer avec succes');
+			}else{
+				$this->session->set_flashdata('error_mdp_change', 'Veuillez verifier votre ancien mot de passe');
+			}
+		} else {
+			$this->session->set_flashdata('error_mdp_change', 'Action non autoris&eacute;e');
+		}
+		redirect('mokapi/mdp_change');
     }
 
 	public function mdp_change()
