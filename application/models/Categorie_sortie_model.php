34323989a6ba0d9733e09e8a7ef3f042218671fe
Jonathan Yombo <jonathanyombo@gmail.com>
2020-01-12 13:16:34 +0200

    feat: add categorie sortie

--- application/models/Categorie_sortie_model.php
+++ application/models/Categorie_sortie_model.php
@@ -1,75 +1,73 @@
 <?php
 
 if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
+	exit('No direct script access allowed');
 
 class Categorie_sortie_model extends CI_Model
 {
 
-    public $table = 'categorie_sortie';
-    public $id = 'id';
-    public $order = 'DESC';
+	public $table = 'categorie_sortie';
+	public $id = 'id';
+	public $order = 'DESC';
 
-    function __construct()
-    {
-        parent::__construct();
-    }
+	function __construct()
+	{
+		parent::__construct();
+	}
 
     // get all
-    function get_all()
-    {
-        $this->db->order_by($this->id, $this->order);
-		$this->db->where('id_utilisateur', $this->session->id);
-        return $this->db->get($this->table)->result();
-    }
+	function get_all()
+	{
+		$this->db->order_by($this->id, $this->order);
+		return $this->db->get($this->table)->result();
+	}
 
     // get data by id
-    function get_by_id($id)
-    {
-        $this->db->where($this->id, $id);
-		$this->db->where('id_utilisateur', $this->session->id);
-        return $this->db->get($this->table)->row();
-    }
+	function get_by_id($id)
+	{
+		$this->db->where($this->id, $id);
+		return $this->db->get($this->table)->row();
+	}
     
     // get total rows
-    function total_rows($q = NULL) {
-        $this->db->like('id', $q);
+	function total_rows($q = null)
+	{
+		$this->db->like('id', $q);
 		$this->db->or_like('id_utilisateur', $q);
 		$this->db->or_like('nom', $q);
-		$this->db->where('id_utilisateur', $this->session->id);
 		$this->db->from($this->table);
-        return $this->db->count_all_results();
-    }
+		return $this->db->count_all_results();
+	}
 
     // get data with limit and search
-    function get_limit_data($limit, $start = 0, $q = NULL) {
-        $this->db->order_by($this->id, $this->order);
-        $this->db->like('id', $q);
+	function get_limit_data($limit, $start = 0, $q = null)
+	{
+		$this->db->order_by($this->id, $this->order);
+		$this->db->like('id', $q);
 		$this->db->or_like('id_utilisateur', $q);
 		$this->db->or_like('nom', $q);
 		$this->db->limit($limit, $start);
-		$this->db->where('id_utilisateur', $this->session->id);
-        return $this->db->get($this->table)->result();
-    }
+		return $this->db->get($this->table)->result();
+	}
 
     // insert data
-    function insert($data)
-    {
-        $this->db->insert($this->table, $data);
-    }
+	function insert($data)
+	{
+		$this->db->insert($this->table, $data);
+	}
 
     // update data
-    function update($id, $data)
-    {
-        $this->db->where($this->id, $id);
-        $this->db->update($this->table, $data);
-    }
+	function update($id, $data)
+	{
+		$this->db->where($this->id, $id);
+		$this->db->update($this->table, $data);
+	}
 
     // delete data
-    function delete($id)
-    {
-        $this->db->where($this->id, $id);
-        $this->db->delete($this->table);
-    }
+	function delete($id)
+	{
+		$this->db->where($this->id, $id);
+		$this->db->delete($this->table);
+	}
 
 }
