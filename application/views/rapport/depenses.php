b3f94df7a9492d55bf2570e871b7a45714f51481
Jonathan Yombo <jonathanyombo@gmail.com>
2020-01-12 13:21:50 +0200

    feat: add depenses

--- application/views/rapport/depenses.php
+++ application/views/rapport/depenses.php
@@ -1,11 +1,14 @@
-<h2 class="flex-center">Rapport</h2>
+<h2 class="flex-center">Rapport depenses : </h2>
 <div class="row">
-	<div class="col-md-3">
-		<?php foreach ($sorties as $sortie):?>
-			<div class="flex-center">
-				<span class="min-chart" id="<?=$sortie->nom?>" data-percent="<?= round(($sortie->montant_utilise / $sortie->seuil) * 100)?>"><span class="percent"></span></span>
-			</div>
-			<h5><span class="label green flex-center"><?=ucfirst($sortie->nom)?> | Solde : <b><?= $sortie->seuil - $sortie->montant_utilise ?></b></span></h5>
-		<?php endforeach; ?>
-	</div>
+    <div class="col-md-3">
+        <?php foreach ($sorties as $sortie):?>
+        <div class="flex-center">
+            <span class="min-chart" id="<?=$sortie->nom?>"
+                data-percent="<?= ($sortie->seuil) ? round(($sortie->montant_utilise / $sortie->seuil) * 100) : 0?>"><span
+                    class="percent"></span></span>
+        </div>
+        <h5><span class="label green flex-center"><?=ucfirst($sortie->nom)?> | Solde :
+                <b><?= $sortie->seuil - $sortie->montant_utilise ?></b></span></h5>
+        <?php endforeach; ?>
+    </div>
 </div>
